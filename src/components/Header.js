6ab7a2dfb02a3218dff1795ec40532002e778209
nhulston <nicholashulston@gmail.com>
2021-12-29 23:35:28 +0800

    Header is done and mobile friendly

---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Link from "next/link";
+import {DiCssdeck} from "react-icons/di";
+import {AiFillGithub, AiFillLinkedin} from "react-icons/ai";
+import styles from '../styles/Header.module.css';
+
+const Header = () => {
+    return (
+        <div className={styles.mainWrapper}>
+            <Link href="/" passHref>
+                <a>
+                    <DiCssdeck size="3rem" className={styles.logo}/>
+                </a>
+            </Link>
+            <div className={styles.linksWrapper}>
+                <li>
+                    <Link href="#projects">
+                        <a className={styles.link}>Projects</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="#skills">
+                        <a className={styles.link}>Skills</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="#about">
+                        <a className={styles.link}>About</a>
+                    </Link>
+                </li>
+            </div>
+            <div className={styles.socialsWrapper}>
+                <a href="https://github.com/nhulston" className={styles.socials} target="_blank" rel="noreferrer">
+                    <AiFillGithub size="3rem" />
+                </a>
+                <a href="https://www.linkedin.com/in/nhulston/" className={styles.socials} target="_blank" rel="noreferrer">
+                    <AiFillLinkedin size="3rem" />
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
